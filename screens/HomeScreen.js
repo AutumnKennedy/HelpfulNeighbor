c4fb1f1094f2d504144595311d3aaf081f2795a9
Kaitlyn Hayes <kaitlyn.hayes-2@selu.edu>
2023-03-05 14:26:32 -0600

    implemented logo on homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,32 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, SafeAreaView, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      // headerTitle: "Helpful Neighbor"
+      headerShown: false
+    })
+  }, [])
+
   return (
-    <View>
-      <Text className="text-red-600">HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text className="text-red-500">
+
+        <View>
+          <Image 
+            source={{
+              uri: "https://www.nicepng.com/png/detail/874-8740891_heart-in-hand-png-clipart-clip-art-social.png"
+            }}
+            className="h-9 w-9 bg-gray-300 p-4"
+          />
+        </View>
+
+      </Text>
+    </SafeAreaView>
   );
 };
 
