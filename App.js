c7f90ae1aaf2fe8d772325e0394cfe7522a7c06b
Kaitlyn Hayes <kaitlyn.hayes-2@selu.edu>
2023-03-07 02:25:46 -0600

    updates

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
-import { NativeBaseProvider, Box } from "native-base";
 
 
 const Stack = createNativeStackNavigator();
